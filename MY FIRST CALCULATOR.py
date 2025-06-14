3eceba6cca2238ff20a691714f48824951e08121
Binit Singh <dgbgaming830@gmail.com>
2025-06-14 13:06:49 +0530

    Add files via upload

---
+++ MY FIRST CALCULATOR.py
@@ -0,0 +1,22 @@
+a=float(int(input("enter the first number:")))
+b=str(input("enter the operator +,*,-,/,**:"))
+c=float(int(input("enter the second number:")))
+if b=="+":
+    print("The answer is:",a+c)
+elif   b=="-":
+    print("The answer is:",a-c)
+elif b=="*" :
+    print("The answer is:",a*c) 
+elif  b=="/":
+    print("The answer is:",a/c)#c!=0
+elif  b=="**":
+    print("The answer is:",a**c)
+  
+  
+print("THANK YOU FOR USING OUR CALCULATOR😁👍")
+
+
+
+
+
+    
